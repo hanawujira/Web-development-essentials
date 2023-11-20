a416fd870fb546e43cffd906e76360c485677000
Hana Wujira <hana.wujira@gebeya.com>
2023-11-20 05:04:08 +0300

    added debugging exercise

---
+++ Lesson 16/script.js
@@ -0,0 +1,24 @@
+
+console.log("Hello, world!")
+
+console.log(message);
+
+greet();
+
+function addNumbers(a, b) {
+  return a + b;
+}
+console.log(addNumbers(5, 10, 15));
+
+let person = {
+  name: "John",
+  age: 30
+};
+console.log(person.address.city);
+
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers[10]);
+
+let x = 10;
+let y = 0;
+console.log(x / y);
