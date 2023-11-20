b5a2ba03d51c2de1c837b14249c483d204be1770
Hana Wujira <hana.wujira@gebeya.com>
2023-11-20 08:31:18 +0300

    localstorage

--- Lesson 16/script.js
+++ Lesson 16/script.js
@@ -22,3 +22,40 @@ console.log(numbers[10]);
 let x = 10;
 let y = 0;
 console.log(x / y);
+
+// the code blow this comment doesn't have errors
+let data = [
+  { country: "USA", name: "John", age: 25 },
+  { country: "Canada", name: "Emily", age: 30 },
+  { country: "UK", name: "James", age: 28 },
+  { country: "Australia", name: "Sophia", age: 32 },
+  { country: "Germany", name: "Max", age: 27 },
+  { country: "France", name: "Emma", age: 29 },
+  { country: "Japan", name: "Hiroshi", age: 31 },
+  { country: "Brazil", name: "Isabella", age: 26 },
+  { country: "India", name: "Aarav", age: 24 },
+  { country: "China", name: "Li", age: 33 },
+  { country: "Mexico", name: "Sofia", age: 29 },
+  { country: "Spain", name: "Mateo", age: 27 },
+  { country: "Italy", name: "Mia", age: 31 },
+  { country: "Russia", name: "Alexander", age: 28 },
+  { country: "South Africa", name: "Liam", age: 26 },
+  { country: "Nigeria", name: "Amara", age: 30 },
+  { country: "Argentina", name: "Santiago", age: 29 },
+  { country: "Egypt", name: "Layla", age: 27 },
+  { country: "Greece", name: "Dimitri", age: 32 },
+  { country: "Sweden", name: "Elsa", age: 25 }
+];
+
+
+// create a function that displays the name, age and country of each person in the data array in the html table
+// the function should be called displayPeople
+// the function should take one parameter, the data array
+// the function should return nothing
+// the function should not have any console.log statements
+// make sure to use forEach to loop through the array
+// make sure to use Object.key to loop through the properties of each object
+
+
+
+
